d12999d85e5281f29f5a854b55e7c1bf86d2bc53
Damien Boizard <boizarddamien751@gmail.com>
2022-12-08 11:18:04 +0100

    All is functionnal

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import pygame as py
 from pygame.locals import *
 from random import randint
 
-#import pyglet
+# import pyglet
+# from pyglet import shapes
+#
+# window = pyglet.window.Window(500, 500)
+# batch = pyglet.graphics.Batch()
+#
+# red_sequare = shapes.Rectangle(150, 240, 200, 20, color=(255, 55, 55), batch=batch)
+# # green_sequare = shapes.Rectangle(175, 220, 150, 20, color=(55, 255, 55), batch=batch)
+# # blue_sequare = shapes.Rectangle(200, 200, 100, 20, color=(55, 55, 255), batch=batch)
+#
+# @window.event
+# def on_draw():
+#     window.clear()
+#     batch.draw()
+#
+# pyglet.app.run()
 
 def applyCell(x, y, mapi):
     aliveNeighbours = 0
@@ -22,15 +37,12 @@ def applyCell(x, y, mapi):
         returnValue = 0 if aliveNeighbours < 2 or aliveNeighbours > 3 else 1
     return returnValue
 
+width = 383
+height = 216
+launched = True
 
 py.init()
-
-width = 384
-height = 216
 fenetre = py.display.set_mode((width * 5, height * 5), FULLSCREEN)
-#fenetre = pyglet.window.Window(width * 5, height * 5, "LifeGame")
-#batch = pyglet.graphics.Batch()
-launched = True
 
 color = [(255, 255, 255), (0, 0, 0)]
 
@@ -38,12 +50,12 @@ map = []
 for h in range(height):
     map.append([])
     for w in range(width):
-        map[h].append(randint(0, 1))
+        # map[h].append(randint(0, 1))
 
-        # if (h*width + w)%2 == 0:
-        #     map[h].append(0)
-        # else:
-        #     map[h].append(1)
+        if (h*width + w)%2 == 0:
+            map[h].append(0)
+        else:
+            map[h].append(1)
 
 while launched:
     for event in py.event.get():
@@ -65,20 +77,3 @@ while launched:
     #py.time.wait(50)
 py.quit()
 
-# @fenetre.event
-# def on_draw():
-#     tempMap = []
-#     for h in range(height):
-#         tempMap.append([])
-#         for w in range(width):
-#             tempMap[h].append(map[h][w])
-#
-#     for h in range(height):
-#         for w in range(width):
-#             pyglet.shapes.Rectangle(w * 5, h * 5, 5, 5, color=color[map[h][w]], batch=batch)
-#             map[h][w] = applyCell(w, h, tempMap)
-#
-#     fenetre.clear()
-#     batch.draw()
-#
-# pyglet.app.run()
